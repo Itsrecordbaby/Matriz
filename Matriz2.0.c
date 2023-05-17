c7f378a4b2540d9a7e1e17d194f6fa8dde13ddb3
Itsrecordbaby <109935521+Itsrecordbaby@users.noreply.github.com>
2023-05-17 15:23:45 -0500

    Add files via upload
    se sigue creando el algoritmo con comentarios

---
+++ Matriz2.0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+   int tamaño;
+
+   printf("Ingrese el tamaño de la matriz: ");
+   scanf("%d", &tamaño);
+
+   int matriz[tamaño][tamaño]
+
+   //lleno la matriz con ceros
+   for (int i = 0; i < tamaño; i++){
+    for (int j=0; j< tamaño; j++){
+        matriz[i][j]=0;
+    }
+   }
+   
+   for
+   
+
+}
